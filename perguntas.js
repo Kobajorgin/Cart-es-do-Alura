2856452e9ec0f273a8e269442cc298eaea646f62
Kobajorgin <kobajorgin@gmail.com>
2024-11-25 16:39:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Programação?',
+    'A programação é o processo de criação de um conjunto de instruções que dizem ao computador como realizar uma tarefa.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Italia?',
+    'A capital da italia é Veneza'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'bom dia em ingles é Good Morning'
+)
